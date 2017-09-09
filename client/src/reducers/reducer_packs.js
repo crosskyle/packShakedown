c52eedd29a59c9cd608c62eb20823b601790f344
crosskyle <kylecross@Kyles-MacBook-Pro.local>
2017-09-09 12:04:46 -0700

    Changed reducer_packs

--- client/src/reducers/reducer_packs.js
+++ client/src/reducers/reducer_packs.js
@@ -1,19 +1,14 @@
 import {CREATE_PACK, READ_PACKS, READ_PACK} from '../actions/types'
-
-//transforms array of objects to object of objects
-const arrayToObject = (array) =>
-  array.reduce((obj, item) => {
-    obj[item.id] = item
-    return obj
-  }, {})
+import _ from 'lodash'
 
 export default function(state = {}, action) {
   switch (action.type) {
     case CREATE_PACK:
       return action.payload
     case READ_PACKS:
-      return action.payload
+      return _.mapKeys(action.payload, "_id")
     case READ_PACK:
+    console.log({...state, [action.payload._id]: action.payload })
       return {...state, [action.payload._id]: action.payload }
     default:
       return state
